fa7454c57ea925383b195a42acbdec640db93dd1
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-12-31 19:45:27 +0100

    added a data module to store the logo picture for the about box dialog

--- src/PicResize.dpr
+++ src/PicResize.dpr
@@ -9,12 +9,14 @@ uses
   Olf.RTL.Params in '..\lib-externes\librairies\Olf.RTL.Params.pas',
   u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
-  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm};
+  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
+  dmPicResizeLogo in 'dmPicResizeLogo.pas' {dmPRLogo: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TdmPRLogo, dmPRLogo);
   Application.Run;
 end.
